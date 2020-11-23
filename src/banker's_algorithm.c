2c35cb9bb2d96a5ffaaba5f03c46d09d51c1e293
残魁斜罡 <ckxgzxa@qq.com>
2020-11-23 22:01:32 +0800

    完成银行家算法

--- src/banker's_algorithm.c
+++ src/banker's_algorithm.c
@@ -1,3 +1,5 @@
+/*简易模拟实现银行家算法*/
+/* 代码地址:https://gitee.com/ZXA200009/operating_system*/
 #include <stdio.h>
 #define TRUE 1
 #define FALSE 0
@@ -11,6 +13,10 @@ int Max[N][M];          // 最大需求矩阵
 int Allocation[N][M];   // 分配矩阵
 int Need[N][M];         // 需求矩阵
 int Request[M];         // 进程的请求向量
+int Work_and_Allocation[N][M];
+int Seq[N],s = 0;   // 记录安全序列
+int Work[N][M],w = 0;    // 工作向量Work
+int Finish[N];  // 指示系统是否有足够的资源分配给进程使之完成
 
 // 初始化资源分配
 void init_status()
@@ -48,8 +54,10 @@ void init_status()
 int cmp_vector(int * a, int * b);
 // 进行两个资源向量的简单运算, 结果存于a中
 void cal_vector(int * a, int * b, char op);
+// 输出分配表信息
+void print_info();
 
-// 安全性算法,判断系统是否处于安全状态,安全返回TRUE
+// 安全性算法,判断系统是否处于安全状态,安全返回TRUE, 否则返回FALSE
 int is_safe();
 // 银行家算法
 void banker_algorithm();
@@ -59,20 +67,24 @@ int main(int argc, char const *argv[])
     printf("--------------简易模拟实现银行家算法--------------\n");
     // 初始化分配
     init_status();
-    printf("功能选择:\n1.判断此种资源分配状态是否安全\n2.判断某进程请求相应资源后系统能否满足其资源请求\n请输入序号选择功能或者键入0退出程序:");
-    int choice;
-    scanf("%d",&choice);
+    // printf("\n功能选择:\n1.判断此种资源分配状态是否安全\n2.判断某进程请求相应资源后系统能否满足其资源请求\n请输入序号选择功能或者键入0退出程序:");
+    int choice = 1; //记录用户选择项
     while(choice)
-    {   
-         switch (choice)
+    {
+        printf("\n功能选择:\n1.判断此种资源分配状态是否安全\n2.判断某进程请求相应资源后系统能否满足其资源请求\n请输入序号选择功能:"); 
+        scanf("%d",&choice);
+        switch (choice)
         {
             case 1:
-                is_safe();
+                if(is_safe()) print_info();
+                else{
+                    printf("此时系统处于不安全状态!");
+                }
                 break;
             case 2:
                 banker_algorithm();
         }
-        printf("功能选择:\n1.判断此种资源分配状态是否安全\n2.判断某进程请求相应资源后系统能否满足其资源请求\n请输入序号选择功能或者键入0退出程序:");
+        printf("是否继续(1/0)?");
         scanf("%d",&choice);
     }
     printf("\n--------------退出系统成功--------------\n");
@@ -84,10 +96,7 @@ int main(int argc, char const *argv[])
 int is_safe()
 {
     int i;
-    int Work_and_Allocation[N][M];
-    int Seq[N],s = 0;   // 如果存在安全序列则记录
-    int Work[N][M],w = 0;    // 工作向量Work
-    int Finish[N];  // 指示系统是否有足够的资源分配给进程使之完成
+
     // 初始化Work向量使之等于Available向量
     for ( i = 0; i < m; ++i)
         Work[w][i] = Available[i];
@@ -122,9 +131,10 @@ int is_safe()
         }
         if (flag2)
         {
-            for ( i = 0; i < n; ++i)
+            for (i = 0; i < n; ++i)
             {
                 if(Finish[i] == FALSE) {
+                    Seq[s++] = i;
                     break;
                 }
             }
@@ -132,34 +142,17 @@ int is_safe()
         }    
     }
     if (i >= n) {
-        printf("系统是安全的, 安全序列如下:\n");
-        printf("进程\tWork\tNeed\tAllocation\tWork+Allocation\n");
-        for(i = 0;i <n;++i)
-        {
-            printf("P%d\t",Seq[i]);
-            for (int j = 0; j < m; ++j)
-                printf("%d ",Work[i][j]);
-            printf("\t");
-            for (int j = 0; j < m; ++j)      
-                printf("%d ",Need[Seq[i]][j]);
-            printf("\t");
-            for (int j = 0; j < m; ++j)      
-                printf("%d ",Allocation[Seq[i]][j]);
-            printf("\t");
-            for (int j = 0; j < m; ++j)      
-                printf("%d ",Work_and_Allocation[i][j]);
-            printf("\n");
-        }
         return TRUE;
     }
-    else return FALSE;    
+    else 
+        return FALSE;    
 }
 
 // 银行家算法
 void banker_algorithm()
 {
     int No;
-    printf("请输入进程号:");
+    printf("\n请输入进程号:");
     scanf("%d",&No);
     printf("请输入进程P%d的请求向量:", No);
     for (int i = 0; i < m; ++i)
@@ -176,7 +169,16 @@ void banker_algorithm()
             cal_vector(Need[No], Request, '-');  
 
             // 随后执行安全性算法, 判断系统是否处于安全状态
-            is_safe();
+            if(is_safe()) {
+                print_info();
+                printf("系统可以满足该进程的资源请求!");
+                printf("\n****************************************\n");                
+            }
+            else{
+                printf("\n****************************************\n");
+                printf("未找到可以避免死锁的安全序列,\n系统无法满足该进程的资源请求!");
+                printf("\n****************************************\n");
+            }
 
             // 恢复最初的资源分配状态
             cal_vector(Available, Request, '+');
@@ -217,4 +219,40 @@ void cal_vector(int * a, int *b, char op)
     default:
         break;
     }
+}
+
+// 系统安全,输出分配表信息
+void print_info()
+{
+    printf("\n****************************************\n");
+    printf("系统是安全的, 模拟分配过程如下:\n");
+    printf("进程\t    Work\t    Need\t  Allocation\t Work+Allocation\tFinish\n");
+    for(int j = 0 ; j < 4; ++j)
+    {
+        printf("\t  ");
+        for(int k = 0 ; k < m; k++)
+            printf("%c  ",(char)('A'+k));
+    }
+    printf("\n");
+    for(int i = 0;i <n;++i)
+    {
+        printf("P%d\t",Seq[i]);
+        for (int j = 0; j < m; ++j)
+            printf("%3d",Work[i][j]);
+        printf("\t");
+        for (int j = 0; j < m; ++j)      
+            printf("%3d",Need[Seq[i]][j]);
+        printf("\t");
+        for (int j = 0; j < m; ++j)
+            printf("%3d",Allocation[Seq[i]][j]);
+        printf("\t");
+        for (int j = 0; j < m; ++j)      
+            printf("%3d",Work_and_Allocation[i][j]);
+        if(Finish[Seq[i]] == TRUE) printf("\t\ttrue\n");
+        else printf("\t\tfalse\n");
+    }
+    printf("安全序列:");
+    for (int j = 0; j < n; ++j)
+        printf("P%d ",Seq[j]);
+    printf("\n****************************************\n");
 }
